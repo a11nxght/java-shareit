0413b151212bf7c781b76d906d4b91a6cf50e778
a11nxght <a11nxght@yandex.kz>
2025-10-16 11:56:11 +0300

    Тестирование Gateway.

---
+++ gateway/src/test/java/ru/practicum/shareit/booking/dto/NewBookingRequestTest.java
@@ -0,0 +1,11 @@
+package ru.practicum.shareit.booking.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewBookingRequestTest {
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ gateway/src/test/java/ru/practicum/shareit/exceptions/ErrorHandlerTest.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit.exceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ErrorHandlerTest {
+    @Test
+    void contextLoads() {
+    }
+}
